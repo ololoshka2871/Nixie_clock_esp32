6137b5b26df9d702b3fd2dae6078994ff4ad5587
tolyan <tolyan@sctbelpa>
2019-08-13 10:56:25 +0300

    update DynamicIndication submodule

--- Nixie_clock_esp32/Program.cs
+++ Nixie_clock_esp32/Program.cs
@@ -16,8 +16,8 @@ namespace Nixie_clock_esp32
 
 			for (int i = 0; i < 2; ++i)
 			{
-				// 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | off
-				// 3 | 2 | 4 | 1 | 8 | 13| 5 | 12| 9 | 0 | 10
+				// 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | off > На лампе
+				// 3 | 2 | 4 | 1 | 8 | 13| 5 | 12| 9 | 0 | 10  > На входе
 				updater.SetData(new uint[] { 3, 2, 4, 1, 8, 13 });
 				Thread.Sleep(3000);
 				updater.SetData(new uint[] { 5, 12, 9, 0, 10, 10 });
